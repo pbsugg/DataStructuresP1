34c104e528bf0c640a20817fd8082e9de9de2e7c
Philip Sugg <pbensugg@gmail.com>
2015-10-29 09:37:54 -0500

    add arraylist type and spec

---
+++ array_list/arraylist_spec.rb
@@ -0,0 +1,35 @@
+require_relative 'arraylist.rb'
+
+describe "ArrayList" do
+
+  context "new" do
+    it "should create a new array of the initial specified size"
+
+    it "should return the array"
+  end
+
+  context "add" do
+    it "should add the element to the end of the list"
+
+    it "should return the element"
+  end
+
+  context "get " do
+    it "should retrieve the element at the specified index"
+  end
+
+  context "set" do
+    it "should take two arguments, for the index position and the element to insert"
+
+  end
+
+  context "size" do
+    it "should return the current size of the list"
+  end
+
+  context "insert(index, element)" do
+    it "should insert the desired element at the specified index"
+  end
+
+
+end
