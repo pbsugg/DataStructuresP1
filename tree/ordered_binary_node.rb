853b85346398817a337ee2508bb55516182279d1
Philip Sugg <pbensugg@gmail.com>
2015-12-03 22:36:32 -0600

    add new ordered version of node, tree to create treeList structure

---
+++ tree/ordered_binary_node.rb
@@ -0,0 +1,24 @@
+# new version of this node for the ordered_list data structure
+
+
+require_relative '../array_list/array_list.rb'
+
+class OrderedBinaryNode
+
+  attr_reader :value, :left_node, :right_node
+
+  def initialize(value)
+    @value = value
+    @left_node = nil
+    @right_node = nil
+  end
+
+  def add_child
+
+  end
+
+  def children
+  end
+
+
+end
---
+++ tree/ordered_tree.rb
@@ -0,0 +1,15 @@
+require_relative 'ordered_binary_node.rb'
+
+class OrderedTree
+
+
+  def initialize
+  end
+
+  def add_node(node)
+  end
+
+  
+
+
+end
